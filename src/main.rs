cba87c9fb68eb6755e86b35a97bd7aca9f6a7aa2
CharaD7 <jijakahn6@gmail.com>
2023-01-17 16:39:54 +0000

    Refactored loop logic

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,21 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number: {}", secret_number);
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
